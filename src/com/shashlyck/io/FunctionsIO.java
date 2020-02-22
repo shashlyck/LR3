357a531ad0cb14b20da1787e8995c7a3b8f774a2
shashlyck <garlodey2@mail.ru>
2020-02-22 19:38:40 +0400

    8

--- src/com/shashlyck/io/FunctionsIO.java
+++ src/com/shashlyck/io/FunctionsIO.java
@@ -1,8 +1,25 @@
 package com.shashlyck.io;
 
+import com.shashlyck.functions.Point;
+import com.shashlyck.functions.TabulatedFunction;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public final class FunctionsIO {
 
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        dataOutputStream.writeInt(function.getCount());
+        for (Point point : function) {
+            dataOutputStream.writeDouble(point.x);
+            dataOutputStream.writeDouble(point.y);
+        }
+        dataOutputStream.flush();
     }
 }
